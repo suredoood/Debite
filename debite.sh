ee57eead656163b2a99e45ac3bd62f4e44f3d810
suredoood <98200221+suredoood@users.noreply.github.com>
2023-09-12 01:22:31 -0700

    Update debite.sh

--- debite.sh
+++ debite.sh
@@ -6,29 +6,13 @@ sudo apt install -y neofetch vim python3 python3-pip wget curl btop flatpak stea
 
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-#### Install Spotify (copied from https://www.spotify.com/de-en/download/linux/)
+#### Install Spotify
 
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo flatpak install flathub com.spotify.Client
 
-sudo apt update && sudo apt install -y spotify-client
+### Install Signal Desktop
 
-#### Install Signal (copied from https://signal.org/download/linux/)
-
-# NOTE: These instructions only work for 64-bit Debian-based
-# Linux distributions such as Ubuntu, Mint etc.
-
-# 1. Install our official public software signing key:
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-
-# 2. Add our repository to your list of repositories:
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee /etc/apt/sources.list.d/signal-xenial.list
-
-# 3. Update your package database and install Signal:
-sudo apt update && sudo apt install -y signal-desktop
-
+sudo flatpak install flathub org.signal.Signal
 
 #### Install Discord
 
