cb0b98dbdffd223a41b412087ccb02cb5b1079ec
suredoood <98200221+suredoood@users.noreply.github.com>
2023-09-12 01:33:36 -0700

    Update debite.sh

--- debite.sh
+++ debite.sh
@@ -17,3 +17,14 @@ sudo flatpak install flathub org.signal.Signal
 #### Install Discord
 
 sudo flatpak install flathub com.discordapp.Discord
+
+
+### Prompt for installing rclone, one-liner from https://rclone.org/install/#linux, prompt from https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
+echo
+echo
+read -p "Do you want to install rclone? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo -v ; curl https://rclone.org/install.sh | sudo bash
+fi
